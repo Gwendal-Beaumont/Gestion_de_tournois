b6b8f3e0a22932ec1aacf3e54a724599f238bea3
vattali <vincent.attali@mines-ales.org>
2021-02-03 11:49:04 +0100

    Petite fonction qui montre le niveau de seum chez la concu en fonction des résultats des campagnes.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,17 @@
 public class Test {
-    public String fonction{
-        String steak = "salé";
-        return steak;
+
+    //Attributs
+    double resultat ;
+    boolean seum ;
+
+    public void setSeum(boolean x){
+        this.seum = x;
+    }
+    public void campagnes(double resultat) {
+        if (resultat > 50) {
+            setSeum(true);
+        } else {
+            setSeum(false);
+        }
     }
 }
