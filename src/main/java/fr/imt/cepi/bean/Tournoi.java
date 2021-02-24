b077f929be3d91764ab7110ef705353273c05f27
vattali <vincent.attali@mines-ales.org>
2021-02-24 10:47:18 +0100

    Renommage des attributs de Tournoi.java

--- src/main/java/fr/imt/cepi/bean/Tournoi.java
+++ src/main/java/fr/imt/cepi/bean/Tournoi.java
@@ -1,3 +1,8 @@
+package fr.imt.cepi.bean;
+
+import fr.imt.cepi.bean.Equipe;
+import fr.imt.cepi.bean.Match;
+
 import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -11,18 +16,18 @@ public class Tournoi implements Serializable {
     private int id;
     private String nom_tournoi;
     private ArrayList<Equipe> liste_equipes;
-    private ArrayList<Match> liste_matches;
+    private ArrayList<Match> ordre_matches;
     private int id_sport;
     private Boolean visibility;
     private LocalDate date;
     private int proprietaire;
 
     // Constructeur
-    public Tournoi(int id, String nom_tournoi, ArrayList<Equipe> liste_equipes, int id_sport, Boolean visibility, LocalDate date, int proprietaire){
+    public Tournoi(int id, String nom_tournoi, ArrayList<Equipe> liste_equipes, int id_sport, Boolean visibility, LocalDate date, int proprietaire) {
         this.id = id;
         this.nom_tournoi = nom_tournoi;
         this.liste_equipes = liste_equipes;
-        liste_matches = new ArrayList<Match>();
+        this.ordre_matches = new ArrayList<>();
         this.id_sport = id_sport;
         this.visibility = visibility;
         this.date = date;
@@ -34,10 +39,9 @@ public class Tournoi implements Serializable {
     /**
      * Setter permettant de définir la liste de matches du tournoi.
      *
-     * @param matches
      */
     public void setMatches(ArrayList<Match> matches) {
-        this.liste_matches = matches;
+        this.ordre_matches = matches;
     }
 
     /**
@@ -45,14 +49,13 @@ public class Tournoi implements Serializable {
      *
      * @return ArrayList
      */
-    public ArrayList getMatches() {
-        return liste_matches;
+    public ArrayList<Match> getMatches() {
+        return ordre_matches;
     }
 
     /**
      * Setter permettant de définir la liste des équipes du tournoi.
      *
-     * @param equipes
      */
     public void setEquipes(ArrayList<Equipe> equipes) {
         this.liste_equipes = equipes;
@@ -63,14 +66,13 @@ public class Tournoi implements Serializable {
      *
      * @return ArrayList
      */
-    public ArrayList getEquipes() {
+    public ArrayList<Equipe> getEquipes() {
         return liste_equipes;
     }
 
     /**
      * Setter permettant de définir le nom du tournoi.
      *
-     * @param nom
      */
     public void setNom_tournoi(String nom) {
         this.nom_tournoi = nom;
@@ -84,6 +86,4 @@ public class Tournoi implements Serializable {
     public String getNom_tournoi() {
         return nom_tournoi;
     }
-
-
 }
