3cc31d9452ff9f396c3bd3c1fabf505c9056beec
gwend <gwendal.beaumont@gmail.com>
2021-03-24 10:46:37 +0100

    RejoindreTournoi fonctionnel

--- src/main/java/fr/imt/cepi/servlet/AddTournamentServlet.java
+++ src/main/java/fr/imt/cepi/servlet/AddTournamentServlet.java
@@ -1,8 +1,6 @@
 package fr.imt.cepi.servlet;
 
-import fr.imt.cepi.bean.Equipe;
-import fr.imt.cepi.bean.Sport;
-import fr.imt.cepi.bean.Tournoi;
+import fr.imt.cepi.bean.RejoindreTournoi;
 import fr.imt.cepi.bean.Utilisateur;
 import fr.imt.cepi.servlet.listeners.AppContextListener;
 import org.apache.logging.log4j.LogManager;
@@ -15,10 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 
 @WebServlet("/AddTournament")
 
@@ -37,53 +32,18 @@ public class AddTournamentServlet extends HttpServlet {
         Utilisateur user = (Utilisateur) req.getSession().getAttribute("utilisateur");
         int iduser = user.getId();
 
-        // On crée nos attributs pour la base de données
-
-        ResultSet rs;
-
-        int numEquipe = 0;
-
-        //Décompte du nombre d'équipe pour en créer une nouvelle avec le bon id
-        try (Connection con = AppContextListener.getConnection(); PreparedStatement ps = con.prepareStatement("SELECT COUNT(*) FROM equipe")) {
-            rs = ps.executeQuery();
-            if(rs.next()) {
-                numEquipe=rs.getInt(1)+1;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        Connection con = null;
+        try {
+            con = AppContextListener.getConnection();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-
-
-        //Insertion de la nouvelle équipe dans le tournoi
-        try (Connection con = AppContextListener.getConnection(); PreparedStatement ps = con.prepareStatement(
-                "INSERT INTO TABLE tournoi_equipe (id_tournoi, id_equipe) VALUES (?,?)")) {
-            ps.setInt(1, idtournoi);
-            ps.setInt(2, numEquipe);
-            ps.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-
-        //Insertion de l'utilisateur dans l'équipe
-
-        try (Connection con = AppContextListener.getConnection(); PreparedStatement ps = con.prepareStatement(
-                "INSERT INTO TABLE equipe_utilisateur (id_equipe,id_joueur) VALUES (?,?)")) {
-            ps.setInt(1, numEquipe);
-            ps.setInt(2, iduser);
-            ps.executeUpdate();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        //Création de l'équipe
-        try (Connection con = AppContextListener.getConnection(); PreparedStatement ps = con.prepareStatement(
-                "INSERT INTO TABLE equipe (nom) VALUES (?)")) {
-            ps.setString(1, "");
-            ps.executeUpdate();
-        } catch (Exception e) {
-            e.printStackTrace();
+        RejoindreTournoi joinTournoi = new RejoindreTournoi(idtournoi, iduser);
+        joinTournoi.join(con);
+        try {
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
 
         getServletContext().getRequestDispatcher("/jsp/add_tournament.jsp").forward(req, resp);
