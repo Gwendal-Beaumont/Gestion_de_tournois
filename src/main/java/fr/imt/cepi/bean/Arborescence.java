8c3748df5b9500debeb49bee4908e78e1e817cf7
dorian-perthuis <dorian.perthuis@outlook.fr>
2021-03-24 10:30:01 +0100

    Ajout de la gestion des exceptions dans la classe arborescence

--- src/main/java/fr/imt/cepi/bean/Arborescence.java
+++ src/main/java/fr/imt/cepi/bean/Arborescence.java
@@ -8,17 +8,20 @@ public class Arborescence {
     ArrayList<Equipe> listeEquipe = new ArrayList<>();
     ArrayList<ArrayList<Match>> listeMatch = new ArrayList<>();
     int id_tournoi;
+
     //Constructeurs
+
 
     public Arborescence(ArrayList<Equipe> listeEquipe, int id_tournoi) {
         this.listeEquipe = listeEquipe;
         this.id_tournoi = id_tournoi;
     }
 
+
     //Methodes
     public void creeArboresence(int nbPlayer) {
         if (Math.floor(Math.log(nbPlayer)) == Math.log(nbPlayer)) {
-            double nbEtape = Math.log(nbPlayer);
+            double nbEtape = Math.log(nbPlayer) ;
             for (int i = 0; i < nbEtape; i++) {
                 ArrayList<Match> Etape = new ArrayList();
                 for (int j = 0; j <= Math.pow(2, nbEtape) - Math.pow(2, i); j++) {
@@ -26,37 +29,41 @@ public class Arborescence {
                 }
             }
         } else {
-            System.out.println("Le nombre d'équipes n'est pas une puissance de 2");
+            throw new IllegalArgumentException("Le nombre d'équipes n'est pas une puissance de 2");
         }
     }
 
-    public void initialisation() {
+    public boolean initialisation() {
         if (listeMatch.isEmpty()) {
-            System.out.println("L'arborescence n'as pas était crée");
+            return false;
         } else {
             int i = 0;
             for (Match m : listeMatch.get(0)) {
                 m.addEquipe(listeEquipe.get(i++));
                 m.addEquipe(listeEquipe.get(i++));
             }
+            return true;
         }
     }
 
-    public void updateArbre() {
+    public boolean updateArbre() {
         if (listeMatch.isEmpty()) {
-            System.out.println("L'arborescence n'a pas été créer");
+            return false;
         } else {
-            for (int i = (int) (Math.log(listeEquipe.size()) - 1); i > 0; i--)
-                for (int j = 0; j < listeMatch.get(i).size(); j++) {
-                    if (listeMatch.get(i).get(j).getEtat() == "Fini") {
-                        listeMatch.get(i + 1).get(j / 2).addEquipe(listeMatch.get(i).get(j).getWinner());
+
+            for (int i = (int)(Math.log(listeEquipe.size())-1); i>0;i--)
+                for (int j = 0; j<listeMatch.get(i).size();j++) {
+                    if(listeMatch.get(i).get(j).getEtat() == "Fini"){
+                        listeMatch.get(i+1).get(j/2).addEquipe(listeMatch.get(i).get(j).getWinner());
                     }
                 }
+            return true;
         }
     }
 
     public void addEquipe(Equipe eq) {
         listeEquipe.add(eq);
     }
+
     //Setter
 }
